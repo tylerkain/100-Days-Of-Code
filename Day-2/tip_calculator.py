3ff0beaa995c8421bcfd2afb6de4649f181ae665
Tyler Kain <tylerkain@Tylers-MacBook-Pro.local>
2023-03-12 11:32:41 -0700

    feat: Day-2 Answers

---
+++ Day-2/tip_calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print("Welcome to Tip Calculator")
+
+number_of_people = int(input("[+] Input number of people: "))
+total_bill = float(input("[+] Input total bill: "))
+tip = int(input("input your tip amount: "))
+
+tip_percentage = float(tip/100)
+tip_amount = total_bill * tip_percentage
+total_bill = (total_bill + tip_amount)/number_of_people
+print(round(total_bill, 2))
