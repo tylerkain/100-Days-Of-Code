604aaf8b4e9389a139bbce643be745edaf97f795
tylerkain <tyler.kain@outlook.com>
2023-03-22 10:54:59 -0500

    refactor: added word list for program

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -1,7 +1,11 @@
-
+#!/usr/bin/env python3
 """Modules"""
 import random
-word_list = ["aardvark", "baboon", "camel"]
+from hangman_art import stages
+from words import word_list
+
+'''Variables'''
+word_list = word_list
 chosen_word = random.choice(word_list)
 display = []
 word_length = len(chosen_word)
@@ -22,7 +26,7 @@ while not game:
         if user_lives == 0:
             game = True
             print("You Lost")
-    print(f"{''.join(display)}")
+    print(f"{''.join(display)} \n Lives Remaining: {user_lives}")
 
     if "_" not in display:
         print("You Win")
