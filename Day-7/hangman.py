58e54d42253896007a2779348941743ba871150f
tylerkain <tyler.kain@outlook.com>
2023-03-22 09:17:43 -0500

    refactor: added logic control

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -2,12 +2,28 @@
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-
 chosen_word = random.choice(word_list)
+display = []
+word_length = len(chosen_word)
+game_lives = 5
+user_lives = 1
+game = False
 
-user_guess = input("Guess a Letter: ").lower()
+for _ in range(word_length):
+    display += "_"
 
-if user_guess in chosen_word:
-    print("Letter is in random word")
-else:
-    print("letter is not in choosen word")
+while not game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+        else:
+            user_lives += 1
+    print(display)
+
+    if "_" not in display:
+        print("You Win")
+        game = True
+
+
