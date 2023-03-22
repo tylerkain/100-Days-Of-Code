76b7b6c686c4fa1dfafdcf05d7d954f283a1e74f
tylerkain <tyler.kain@outlook.com>
2023-03-22 09:59:02 -0500

    refactor: added new file for ascii art

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -1,12 +1,11 @@
+
 """Modules"""
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 display = []
 word_length = len(chosen_word)
-game_lives = 5
-user_lives = 1
+user_lives = 6
 game = False
 
 for _ in range(word_length):
@@ -18,12 +17,18 @@ while not game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-        else:
-            user_lives += 1
-    print(display)
+    if guess not in chosen_word:
+        user_lives -= 1
+        if user_lives == 0:
+            game = True
+            print("You Lost")
+    print(f"{''.join(display)}")
 
     if "_" not in display:
         print("You Win")
         game = True
+
+    print(stages[user_lives])
+
 
 
