0d78d7a9ad8f5d7803e35c9f4d61a50c56099247
tylerkain <tyler.kain@outlook.com>
2023-03-25 09:59:06 -0500

    feat: added new program

---
+++ Day-8/caesar-cipher.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text=text, shift_amount=shift):
+    """Encrypt Message"""
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f" the encoded message is: {cipher_text}")
+
+
+def decrypt(cipher_text=text, shift_amount=shift):
+    """Decrypt Message"""
+    plain_text = " "
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        plain_text += new_letter
+    print(f"the decoded message is: {plain_text}")
+
+
+if direction == 'encode':
+    encrypt()
+if direction == 'decode':
+    decrypt()
