887de83b1b38e017e3c13d33a4635965b5fdd47c
tylerkain <tyler.kain@outlook.com>
2023-04-02 10:32:45 -0500

    feat: added number guessing game main.py

---
+++ Day-12/main.py
@@ -0,0 +1,4 @@
+#!/bin/usr/env python3
+from number_guess import game
+
+game()
---
+++ Day-12/number_guess.py
@@ -0,0 +1,26 @@
+#!/bin/usr/env python3
+import random
+
+
+def check_guess(user_answer, computer_answer):
+    """Check guess based on level of difficulty"""
+    if user_answer > computer_answer:
+        print("Guess is to High")
+    elif user_answer < computer_answer:
+        print("Guess is to Low")
+    else:
+        print(f"You got the answer:{computer_answer}")
+
+
+def game():
+    answer = random.randint(1, 100)
+    print("Guess a number between 1-100: ")
+    user_guess = 0
+    while user_guess != answer:
+        user_guess = int(input("Make a guess: "))
+        check_guess(user_guess, answer)
+
+
+game()
+
+
