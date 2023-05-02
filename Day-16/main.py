0fcb1da0cead5aaba1cfb1100004802349ca48cc
tylerkain <tyler.kain@outlook.com>
2023-05-02 08:22:24 -0500

    feat: added day-17 directory

--- Day-16/main.py
+++ Day-16/main.py
@@ -17,7 +17,7 @@ while is_on:
         coffee_maker.report()
         money_machine.report()
     else:
-        drink = menu.get_items()
+        drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
 
